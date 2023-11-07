21df2ab28687304918b29b0b604773747c061438
olegstan <olegstan@inbox.ru>
2023-11-08 02:36:48 +0300

    export

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import Button from "./Form/Button";
+import Checkbox from "./Form/Checkbox";
+import Date from "./Form/Date";
+import DateTime from "./Form/DateTime";
+import File from "./Form/File";
+import Form from "./Form/Form";
+import Input from "./Form/Input";
+import MaskedInput from "./Form/MaskedInput";
+import ModalForm from "./Form/ModalForm";
+import NumberInput from "./Form/NumberInput";
+import SlimInput from "./Form/SlimInput";
+import TextArea from "./Form/TextArea";
+
+import AccountGroupSelect from "./Form/AccountGroupSelect/AccountGroupSelect";
+import SlimAccountGroupSelect from "./Form/AccountGroupSelect/SlimAccountGroupSelect";
+
+import GroupMultipleSelect from "./Form/GroupMultipleSelect/GroupMultipleSelect";
+
+import GroupRemoteSearch from "./Form/GroupSearch/GroupRemoteSearch";
+
+import GroupSelect from "./Form/GroupSelect/GroupSelect";
+
+import InputPopup from "./Form/InputPopup/InputPopup";
+
+import InputWithCurrency from "./Form/InputWithCurrency/InputWithCurrency";
+
+import MultipleSelect from "./Form/MultipleSelect/MultipleSelect";
+
+import RelationGroupSelect from "./Form/RelationGroupSelect/RelationGroupSelect";
+
+import Search from "./Form/Search/Search";
+import RemoteSearch from "./Form/Search/RemoteSearch";
+
+import SearchMultiple from "./Form/SearchMultiple/SearchMultiple";
+
+import SearchWithButton from "./Form/SearchWithButton/SearchWithButton";
+
+import Select from "./Form/Select/Select";
+
+import SelectWithDays from "./Form/SelectWithDays/SelectWithDays";
+
+import formAccountGroupSelect from "./Form/functions/formAccountGroupSelect"
+import formCheckbox from "./Form/functions/formCheckbox"
+import formDate from "./Form/functions/formDate"
+import formFile from "./Form/functions/formFile"
+import formInput from "./Form/functions/formInput"
+import formSearch from "./Form/functions/formSearch"
+import formSelect from "./Form/functions/formSelect"
+import getLink from "./Form/functions/getLink"
+import receivePropsUserId from "./Form/functions/receivePropsUserId"
+import setField from "./Form/functions/setField"
+
+
+export {
+  Button,
+  Checkbox,
+  Date,
+  DateTime,
+  File,
+  Form,
+  Input,
+  MaskedInput,
+  ModalForm,
+  NumberInput,
+  SlimInput,
+  TextArea,
+
+  AccountGroupSelect,
+  SlimAccountGroupSelect,
+
+  GroupMultipleSelect,
+
+  GroupRemoteSearch,
+
+  GroupSelect,
+
+  InputPopup,
+
+  InputWithCurrency,
+
+  MultipleSelect,
+
+  RelationGroupSelect,
+
+  Search,
+  RemoteSearch,
+
+  SearchMultiple,
+
+  SearchWithButton,
+
+  Select,
+
+  SelectWithDays,
+
+  formAccountGroupSelect,
+  formCheckbox,
+  formDate,
+  formFile,
+  formInput,
+  formSearch,
+  formSelect,
+  getLink,
+  receivePropsUserId,
+  setField,
+};
