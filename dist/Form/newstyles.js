501bb02951fdbd85331a6303e0fdd2ac8ed549bd
olegstan <olegstan@inbox.ru>
2023-11-20 21:49:38 +0300

    fix form

--- dist/Form/newstyles.js
+++ dist/Form/newstyles.js
@@ -132,11 +132,7 @@ export const sharedCheckboxStyle = css`
   }
 
   .checkbox {
-    margin: 7px 5px 0 0;
     cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
     user-select: none;
     display: flex;
   }
