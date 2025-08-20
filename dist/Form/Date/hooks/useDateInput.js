0e78b227ff1ccfc17a51ad55f59d66d3b5256dac
olegstan <olegstan@inbox.ru>
2025-08-20 17:51:02 +0300

    fix date

--- dist/Form/Date/hooks/useDateInput.js
+++ dist/Form/Date/hooks/useDateInput.js
@@ -13,10 +13,40 @@ var _moment = _interopRequireDefault(require("moment"));function _interopRequire
 
 
 
+// Utility function to safely check if a value is a valid Date
+var isValidDate = function isValidDate(date) {
+  return date instanceof Date && !isNaN(date.getTime());
+};
+
+// Utility function to normalize input to Date | null
+var normalizeDate = function normalizeDate(value) {
+  if (value === null || value === undefined) {
+    return null;
+  }
+
+  if (isValidDate(value)) {
+    return value;
+  }
+
+  // If it's a string, try to parse it
+  if (typeof value === 'string') {
+    var parsed = new Date(value);
+    return isValidDate(parsed) ? parsed : null;
+  }
+
+  // For any other type, return null
+  return null;
+};
+
 var isSameDateTime = function isSameDateTime(a, b) {
-  if (!a && !b) return true;
-  if (!a || !b) return false;
-  return a.getTime() === b.getTime();
+  // Normalize both inputs
+  var dateA = normalizeDate(a);
+  var dateB = normalizeDate(b);
+
+  if (!dateA && !dateB) return true;
+  if (!dateA || !dateB) return false;
+
+  return dateA.getTime() === dateB.getTime();
 };
 
 var useDateInput = exports.useDateInput = function useDateInput(_ref)
@@ -30,38 +60,44 @@ var useDateInput = exports.useDateInput = function useDateInput(_ref)
 
 
 {var value = _ref.value,onChange = _ref.onChange,_ref$onBlur = _ref.onBlur,onBlur = _ref$onBlur === void 0 ? function () {} : _ref$onBlur,flatpickrInstance = _ref.flatpickrInstance,_ref$setFocused = _ref.setFocused,setFocused = _ref$setFocused === void 0 ? function () {} : _ref$setFocused,dateFormat = _ref.dateFormat,dateMask = _ref.dateMask,formatDate = _ref.formatDate,_ref$setInnerError = _ref.setInnerError,setInnerError = _ref$setInnerError === void 0 ? function () {} : _ref$setInnerError;
-  var _useState = (0, _react.useState)(value !== null && value !== void 0 ? value : null),_useState2 = _slicedToArray(_useState, 2),date = _useState2[0],setDate = _useState2[1];
-  var _useState3 = (0, _react.useState)(formatDate(value !== null && value !== void 0 ? value : null, dateFormat)),_useState4 = _slicedToArray(_useState3, 2),dateString = _useState4[0],setDateString = _useState4[1];
+  // Normalize the initial value
+  var normalizedValue = normalizeDate(value);
+
+  var _useState = (0, _react.useState)(normalizedValue),_useState2 = _slicedToArray(_useState, 2),date = _useState2[0],setDate = _useState2[1];
+  var _useState3 = (0, _react.useState)(formatDate(normalizedValue, dateFormat)),_useState4 = _slicedToArray(_useState3, 2),dateString = _useState4[0],setDateString = _useState4[1];
 
   // последнее отданное наружу значение, чтобы не дублировать onChange
-  var lastEmittedRef = (0, _react.useRef)(value !== null && value !== void 0 ? value : null);
+  var lastEmittedRef = (0, _react.useRef)(normalizedValue);
 
   // синхронизация при внешнем изменении value
   (0, _react.useEffect)(function () {
-    if (!isSameDateTime(value !== null && value !== void 0 ? value : null, date)) {
-      setDate(value !== null && value !== void 0 ? value : null);
-      setDateString(formatDate(value !== null && value !== void 0 ? value : null, dateFormat));
+    var normalizedValue = normalizeDate(value);
+
+    if (!isSameDateTime(normalizedValue, date)) {
+      setDate(normalizedValue);
+      setDateString(formatDate(normalizedValue, dateFormat));
     }
-    if (!isSameDateTime(value !== null && value !== void 0 ? value : null, lastEmittedRef.current)) {
-      lastEmittedRef.current = value !== null && value !== void 0 ? value : null;
+    if (!isSameDateTime(normalizedValue, lastEmittedRef.current)) {
+      lastEmittedRef.current = normalizedValue;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value, dateFormat]);
 
   var emitIfChanged = function emitIfChanged(next) {
-    if (!isSameDateTime(next, lastEmittedRef.current)) {
-      lastEmittedRef.current = next;
-      onChange(next);
+    var normalizedNext = normalizeDate(next);
+    if (!isSameDateTime(normalizedNext, lastEmittedRef.current)) {
+      lastEmittedRef.current = normalizedNext;
+      onChange(normalizedNext);
     }
   };
 
   // Клик по календарю / изменение от flatpickr
   var handleDateChange = function handleDateChange(selectedDates) {
-    var next = selectedDates && selectedDates.length ? selectedDates[0] : null;
+    var next = selectedDates && selectedDates.length ? normalizeDate(selectedDates[0]) : null;
 
     setDate(next);
     setDateString(formatDate(next, dateFormat));
-    setInnerError(false); // было: null
+    setInnerError(false);
 
     emitIfChanged(next);
   };
@@ -73,7 +109,7 @@ var useDateInput = exports.useDateInput = function useDateInput(_ref)
 
     if (raw.trim() === '') {
       setDate(null);
-      setInnerError(false); // было: null
+      setInnerError(false);
       emitIfChanged(null);
       return;
     }
@@ -81,9 +117,10 @@ var useDateInput = exports.useDateInput = function useDateInput(_ref)
     var parsed = (0, _moment["default"])(raw, dateFormat, true);
     if (parsed.isValid()) {
       var next = parsed.toDate();
-      setDate(next);
-      setInnerError(null); // было: null
-      emitIfChanged(next);
+      var normalizedNext = normalizeDate(next);
+      setDate(normalizedNext);
+      setInnerError(null);
+      emitIfChanged(normalizedNext);
     } else {
       console.log("\u041D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 \u0444\u043E\u0440\u043C\u0430\u0442 \u0434\u0430\u0442\u044B: ".concat(raw));
     }
@@ -93,7 +130,7 @@ var useDateInput = exports.useDateInput = function useDateInput(_ref)
     if (dateString && !(0, _moment["default"])(dateString, dateFormat, true).isValid()) {
       setDate(null);
       setDateString('');
-      setInnerError(false); // было: null
+      setInnerError(false);
       emitIfChanged(null);
     }
     onBlur === null || onBlur === void 0 || onBlur();
